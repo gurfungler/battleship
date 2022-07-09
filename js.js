f6608c46f4b174e1995ea309c054b6a70a70f59d
Mickey Munsey <benidict92@gmail.com>
2022-07-09 14:02:53 -0400

    player attack,cpu attack, started designing how ships are created, look into infinite loop

--- js.js
+++ js.js
@@ -31,15 +31,29 @@ const shipFactory = (length, n) => {
 };
 
 const gameboardFactory = () => {
-  let board = Array(10);
-  for (let i = 0; i < 10; ++i) {
-    board[i] = new Array(10);
-    board[i].fill("-");
-  }
+  const makeBoard = () => {
+    let board = Array(10);
+    for (let i = 0; i < 10; ++i) {
+      board[i] = new Array(10);
+      board[i].fill("-");
+    }
+
+    return board;
+  };
+  const makeShips = (s) => {
+    for (let i = 0; i < 5; ) {
+      s[i] = shipFactory(i, i);
+    }
+  };
+  let ships = new Array(5);
+  let board = makeBoard();
+  makeShips(ships);
   return {
+    ships,
     board,
     placeShip(ship, startX, startY, direction) {
-      if (checkOverFlow(ship, startX, startY, direction)) {
+      let c = 0;
+      if (this.checkOverFlow(ship, startX, startY, direction)) {
         if (direction == 0) {
           for (let i = 0; i < ship.length; ++i) {
             board[startY + i][startX] = ship.name;
@@ -67,11 +81,41 @@ const gameboardFactory = () => {
         } else {
           board[y][x] = "x";
         }
+      } else {
+        board[y][x] = "a";
       }
     },
   };
 };
+
+const playerFactory = () => {
+  return {
+    playerBoard: gameboardFactory(),
+    cpuHit(board) {
+      let x;
+      let y;
+      do {
+        x = Math.floor((Math.random() % 10) * 10);
+        y = Math.floor((Math.random() % 10) * 10);
+      } while (board.board[x][y] == "x" || board.board[x][y] == "m");
+      this.hit(x, y, board);
+    },
+    hit(x, y, board) {
+      board.recieveAttack(x, y);
+    },
+  };
+};
+const game = () => {
+  let cpu = new playerFactory();
+  let player = new playerFactory();
+  let turn = 0;
+  // do {
+  // if (turn == 0) {
+  // }
+  // } while (condition);
+};
 module.exports = {
   shipFactory: shipFactory,
   gameboardFactory: gameboardFactory,
+  playerFactory: playerFactory,
 };
--- ship.test.js
+++ ship.test.js
@@ -1,7 +1,7 @@
 const modules = require("./js");
 let shipF = modules.shipFactory;
 let gameboardF = modules.gameboardFactory;
-
+let playerF = modules.playerFactory;
 it("ship make", () => {
   let ship = shipF(1, "s1");
   expect(ship.name).toBe("s1");
@@ -73,12 +73,37 @@ it("gameboard hit already", () => {
   let ship = shipF(3, "s1");
   let gameboard = gameboardF();
   gameboard.placeShip(ship, 0, 0, 0);
-  expect(gameboard.board[0][0]).toBe("Already hit");
+  gameboard.recieveAttack(0, 0);
+  gameboard.recieveAttack(0, 0);
+  expect(gameboard.board[0][0]).toBe("a");
 });
 
 it("gameboard hit miss", () => {
-  let ship = shipF(3, "s1");
+  let ship = shipF(1, "s1");
   let gameboard = gameboardF();
-  gameboard.placeShip(ship, 0, 0, 0);
+  gameboard.placeShip(ship, 1, 1, 0);
+  gameboard.recieveAttack(0, 0);
   expect(gameboard.board[0][0]).toBe("m");
+});
+
+it("player attack", () => {
+  let ship = shipF(1, "s1");
+  let gameboard = gameboardF();
+  let player = playerF(0);
+  gameboard.placeShip(ship, 0, 0, 0);
+  player.hit(0, 0, gameboard, 1);
+  expect(gameboard.board[0][0]).toBe("x");
+});
+
+it("CPU attack", () => {
+  let gameboard = gameboardF();
+  let player = playerF(0);
+  player.cpuHit(gameboard);
+  let check = false;
+  for (let i = 0; i < 10; ++i) {
+    if (gameboard.board[i].includes("m")) {
+      check = true;
+    }
+  }
+  expect(check).toBe(true);
 });
